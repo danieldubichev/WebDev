0b3835426a1b21424fe965a740c9c8f970d72240
danieldubichev <dannydubichev@gmail.com>
2020-05-18 15:50:12 -0700

    Components & props finished

--- my-app/src/CompsAndProps.jsx
+++ my-app/src/CompsAndProps.jsx
@@ -88,7 +88,70 @@ function Comment(props) {
       </div>
     );
   }
+  */
+
+//It accepts 'author' (an object)
+// 'text' (a string)
+// 'date' (a date) all as props
+// it describes a comment on a social media website
+
+/*
+  This component can be tricky to change because of all the nesting,
+  and it it also hard to reuse individual parts of it.
+  So, let's extract a few components from it. First, we will extract Avatar:
 */
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+//The Avatar doesnt need to know that it is being rendered inside a comment. This is why we have given its prop a more generic name: user, rather than author
+//We recommend naming props from the components own point of view rather than the context in which it is being used
+
+//Next, we will extract a UserInfo componenet that renders an Avatar next to the users name
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+//This lets us simplify comment to this extent:
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+//extracting components might seem like grunt work at first, but having a palette of reusable componenets pays off in larger apps.
+//a good rule of thumb is that if a part of your UI is ised several times (Button, Panel, Avatar),
+//or is complex enough on its own (App, FeedStory, Comment), it is a good candidate to be a reusable component.
+
+const comment = {
+  date: new Date(),
+  text: 'What a time to be alive',
+  author: {
+    name: 'JoeBob123',
+    avatarUrl: 'https://i.imgflip.com/3dhqb0.jpg',
+  },
+};
 
 class CompsAndProps extends React.Component {
     render() {
@@ -97,6 +160,11 @@ class CompsAndProps extends React.Component {
                     {element1}
                     {element2}
                     {App1()}
+                    <Comment
+                      date={comment.date}
+                      text={comment.text}
+                      author={comment.author}
+                    />,
             </div>
         );    
 }
